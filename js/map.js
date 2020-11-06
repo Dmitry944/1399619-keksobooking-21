41a0e6d5910fb8bf8869a922d8c82f2097ed328f
Dmitry <dimon944@bk.ru>
2020-11-07 02:16:44 +0300

    перетаскивание пина

--- js/map.js
+++ js/map.js
@@ -2,12 +2,26 @@
 
 (function () {
   const ESC_KEY = window.consts.ESC_KEY;
+  const MAIN_PIN_ARROW = window.consts.MAIN_PIN_ARROW;
 
   const map = window.elements.map;
   const mapPins = window.elements.mapPins;
+  const mapPinMain = window.elements.mapPinMain;
   const mapFilters = window.elements.mapFilters;
   const createCardElement = window.card.createCardElement;
   const offerObjectsArr = window.data.createOffersArray();
+  const setAddress = window.form.setAddress;
+
+  const Coordinates = {
+    x: {
+      min: 0,
+      max: map.offsetWidth
+    },
+    y: {
+      min: 130,
+      max: 630
+    }
+  };
 
   function appendPins() {
     const offerObject = document.createDocumentFragment();
@@ -62,8 +76,60 @@
     }
   }
 
+  function mainPinMove(evt) {
+    evt.preventDefault();
+
+    let startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    const onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      const shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + `px`;
+      mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + `px`;
+
+      if (mapPinMain.offsetLeft < Coordinates.x.min - mapPinMain.offsetWidth / 2) {
+        mapPinMain.style.left = (Coordinates.x.min - mapPinMain.offsetWidth / 2) + `px`;
+      } else if (mapPinMain.offsetLeft > Coordinates.x.max - mapPinMain.offsetWidth / 2) {
+        mapPinMain.style.left = (Coordinates.x.max - mapPinMain.offsetWidth / 2) + `px`;
+      }
+
+      if (mapPinMain.offsetTop < Coordinates.y.min - mapPinMain.offsetHeight - MAIN_PIN_ARROW) {
+        mapPinMain.style.top = Coordinates.y.min - mapPinMain.offsetHeight - MAIN_PIN_ARROW + `px`;
+      } else if (mapPinMain.offsetTop > Coordinates.y.max - mapPinMain.offsetHeight - MAIN_PIN_ARROW) {
+        mapPinMain.style.top = Coordinates.y.max - mapPinMain.offsetHeight - MAIN_PIN_ARROW + `px`;
+      }
+
+      setAddress(true);
+    };
+
+    const onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener(`mousemove`, onMouseMove);
+      document.removeEventListener(`mouseup`, onMouseUp);
+    };
+
+    document.addEventListener(`mousemove`, onMouseMove);
+    document.addEventListener(`mouseup`, onMouseUp);
+  }
+
   window.map = {
     appendPins,
-    pinClickHandler
+    pinClickHandler,
+    mainPinMove
   };
+
 })();
